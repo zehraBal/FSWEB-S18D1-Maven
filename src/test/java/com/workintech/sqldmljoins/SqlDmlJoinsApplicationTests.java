f20458b55344be91d8e972dd5280b075dce0046e
Dogancan Kınık <dogancankinik@gmail.com>
2024-04-17 19:31:16 +0300

    Update SqlDmlJoinsApplicationTests.java

--- src/test/java/com/workintech/sqldmljoins/SqlDmlJoinsApplicationTests.java
+++ src/test/java/com/workintech/sqldmljoins/SqlDmlJoinsApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
 import java.util.stream.Collectors;
